aa4c59e32d2c24a307d2bfc3d78284dc336b1247
Naveen Basavaraj <naveen.b.workmail@gmail.com>
2025-05-15 15:42:23 +0000

    top k freq elements

--- 1_array_and_hashing/5_top_k_freq.py
+++ 1_array_and_hashing/5_top_k_freq.py
@@ -1,3 +1,17 @@
 class FreqElements:
     def topKfreq(self, nums):
-        pass
+        count = {}
+        freq = [[] for i in range(len(nums)+1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        
+        for num in count:
+            i = count[num]
+            freq[i].append(num)
+        
+        for i in range(len(freq)-1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
