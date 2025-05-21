117481125a5d6519ba83d767702289a96f03db6b
Naveen Basavaraj <naveen.b.workmail@gmail.com>
2025-05-21 05:45:42 +0000

    two sum

--- 4_two_pointers/2_three_sum.py
+++ 4_two_pointers/2_three_sum.py
@@ -11,15 +11,19 @@ class ThreeSum:
             l, r = i + 1, len(nums) - 1
             while l < r:
                 threeSum = a + nums[l] + nums[r]
-            if threeSum > 0:
-                    r -= 1
-            elif threeSum < 0:
-                l += 1
-            else:
-                res.append([a, nums[l], nums[r]])
-                l += 1
-                r -= 1
-                while nums[l] == nums[l - 1] and l < r:
+                if threeSum > 0:
+                        r -= 1
+                elif threeSum < 0:
                     l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
                         
         return res
+
+if __name__ == "__main__":
+    threes = ThreeSum()
+    print(threes.usingTwoPointers([-3,0,4,1,2]))
