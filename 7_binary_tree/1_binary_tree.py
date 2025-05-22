44f5d2935782fb82f26a90f8f59bb10d44e53e6d
Naveen Basavaraj <naveen.b.workmail@gmail.com>
2025-05-22 14:34:57 +0000

    binary tree

--- 7_binary_tree/1_binary_tree.py
+++ 7_binary_tree/1_binary_tree.py
@@ -16,3 +16,33 @@ class BinarySearchTree:
         elif value > node.value:
             node.right = _insert(node.right, value)
         return node
+    
+    def search(self, node, target):
+        if not node:
+            return False
+    
+        if target > node.val:
+            return search(node.right, target)
+        elif target < node.val:
+            return search(node.left, target)
+        else:
+            return True
+    
+    def remove(node, val):
+        if not node:
+            return None
+
+        if val > node.val:
+            node.right = remove(node.right, val)
+        elif val < node.val:
+            node.left = remove(node.left, val)
+        else:
+            if not node.left:
+                return node.right
+            elif not node.right:
+                return node.left
+            else:
+                minNode = minValueNode(node.right)
+                node.val = minNode.val
+                node.right = remove(node.right, minNode.val)
+        return node
