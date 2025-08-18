248874eacdc189dcfa262b8c481b95d3e1eaa13d
Naveen Basavaraj <naveen.b.workmail@gmail.com>
2025-08-18 15:10:03 +0000

    stack

--- neetcode/1_stack/valid_parentheses.py
+++ neetcode/1_stack/valid_parentheses.py
@@ -1,4 +1,16 @@
 class ValidParentheses:
-    def __init__(self):
-        pass
+    def solution(self, s):
+        stack = []
+        close_to_open_map = {")":"(", "}":"{", "]":"["}
+
+        for ch in s:
+            if ch in close_to_open_map: 
+                if stack and stack[-1] == close_to_open_map[ch]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append()
+            
+
     
